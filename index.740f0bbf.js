163ad6f3379415c66cf3adf2530befaed0289e93
Anastasia Potapova <stasya7776@yandex.ru>
2022-04-13 11:43:40 +0300

    added files

---
+++ index.740f0bbf.js
@@ -0,0 +1,2 @@
+document.body.onload=function(){setTimeout((function(){var e=document.getElementById("page-preloader");e.classList.contains("done")||e.classList.add("done")}),1e3)};
+//# sourceMappingURL=index.740f0bbf.js.map
